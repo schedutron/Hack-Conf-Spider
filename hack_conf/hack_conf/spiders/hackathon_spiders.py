560bbeb644be55e20f095b4d4cbdc57e108f9829
Saurabh S. Chaturvedi <saurabh.chaturvedi63@gmail.com>
2017-08-14 19:27:41 +0530

    Make hackathon spider PEP8 compliant

--- hack_conf/hack_conf/spiders/hackathon_spiders.py
+++ hack_conf/hack_conf/spiders/hackathon_spiders.py
@@ -1,14 +1,17 @@
-"""Spiders for scraping hackathons' information."""
+"""Spiders for scraping hackathons' information.
+
+* Think about converting time from a string to a time object.
+* If description is short enough, it will be added to subtitle, and description
+value would be None.
+* If prize is null, it need not imply that there are no prizes; one can always
+visit the hackathon link for more info.
+"""
 
 from bs4 import BeautifulSoup  # Eventually migrate to lxml.
 from lxml import html
 import scrapy
 from hack_conf.items import HackData  # Imports metadata.
 import re
-
-#think about converting time from a string to a time object
-#if description is short enough, it will be added to subtitle, and description value would be None
-#if prize is null, it need not imply that there are no prizes; one can always visit the hackathon link for more info
 
 
 class HackathonDotComSpider(scrapy.Spider):
@@ -18,12 +21,18 @@ class HackathonDotComSpider(scrapy.Spider):
     ]
 
     def parse(self, response):
-        block = response.xpath("body/div[@class='ht-page__content']/div[@class='row']")[1]
-        cities_links = block.xpath(".//a[@class='ht-all-card']/@href").extract()
+        block = response.xpath(
+            "body/div[@class='ht-page__content']/div[@class='row']"
+        )[1]
+        cities_links = block.xpath(
+            ".//a[@class='ht-all-card']/@href"
+        ).extract()
         for city_link in cities_links:
             yield response.follow(city_link, callback=self.parse_final)
-        yield response.follow("https://www.hackathon.com/online", callback=self.parse_final)
-
+        yield response.follow(
+            "https://www.hackathon.com/online",
+            callback=self.parse_final
+        )
 
     def parse_final(self, response):
         block = response.xpath("//div[@class='row align-center']")[0]
@@ -31,33 +40,43 @@ class HackathonDotComSpider(scrapy.Spider):
 
         def parse_hackathonDotcom(ele):
             data = HackData()
-            #data from this source contains image as well, but presently images aren't supported here. Maybe later they will be.
+            # Data from this source contains image as well, but presently
+            # images aren't supported here. Maybe later they will be.
             data['source'] = 'http://www.hackathon.com'
 
             try:
-                data['location'] = ele.xpath(".//span[@class='ht-eb-card__location__place']/text()")[0].strip()
+                data['location'] = ele.xpath(
+                    ".//span[@class='ht-eb-card__location__place']/text()"
+                )[0].strip()
             except Exception:
                 pass
 
             try:
-                title_selector = ele.xpath(".//a[@class='ht-eb-card__title']")[0]
+                title_selector = ele.xpath(
+                    ".//a[@class='ht-eb-card__title']"
+                )[0]
                 data['title'] = title_selector.xpath("text()")[0].strip()
             except Exception:
                 pass
 
             try:
-                data['link'] = title_selector.attrib["href"] #scrapes out link about hackathon, link which is href of title
+                # Scrapes out link (href) about the hackathon.
+                data['link'] = title_selector.attrib["href"]
             except Exception:
                 pass
 
             try:
-                data['description'] = ele.xpath(".//div[@class='ht-eb-card__description']/text()")[0].strip() #scrapes out hackathon description
-                #description perhaps not clean! contains \n and stuff (probably unicode stuff)
+                # Description perhaps not clean! contains \n and stuff.
+                # (probably unicode stuff)
+                data['description'] = ele.xpath(
+                    ".//div[@class='ht-eb-card__description']/text()"
+                    )[0].strip()  # Scrapes out hackathon description.
             except Exception:
                 pass
 
-            try: #this block scrapes time of hackathon
-                time = ele.xpath(".//div[@class='ht-eb-card__date ']")[0].text_content()
+            try:  # This block scrapes time of hackathon.
+                time = ele.xpath(".//div[@class='ht-eb-card__date ']")[0]
+                time = time.text_content()
                 index = time.find("Ends")
                 if index != -1:
                     time = time[:index] + ", Ends" + time[index+4:]
@@ -65,13 +84,16 @@ class HackathonDotComSpider(scrapy.Spider):
             except Exception:
                 pass
 
-            try: #this block scrapes tags (if available)
-                data['tags'] = ele.xpath(".//div[@class='ht-card-tags']")[0].xpath("./a/text()")
+            try:  # This block scrapes tags (if available).
+                data['tags'] = ele.xpath(".//div[@class='ht-card-tags']")[0]
+                data['tags'] = data['tags'].xpath("./a/text()")
             except Exception:
                 pass
 
             try:  # This gets the hackathon's prize info.
-                data['prize'] = ele.xpath(".//div[@class='ht-eb-card__prize__name']/text()")[0].strip()
+                data['prize'] = ele.xpath(
+                    ".//div[@class='ht-eb-card__prize__name']/text()"
+                    )[0].strip()
             except Exception:
                 pass
 
@@ -80,6 +102,8 @@ class HackathonDotComSpider(scrapy.Spider):
         for hackathon in city_hacks:
             yield parse_hackathonDotcom(html.fromstring(hackathon.extract()))
 
-        more_button = response.xpath("//a[@data-pagination-more-link='hackathons']")
+        more_button = response.xpath(
+            "//a[@data-pagination-more-link='hackathons']"
+        )
         while more_button:
             yield response.follow(more_button[0], callback=self.parse_final)
